84559a10f320888742ccec055a06919fb639f859
ramirosimari67 <148803447+ramirosimari67@users.noreply.github.com>
2024-04-26 00:39:11 -0300

    Add files via upload

---
+++ Programación-Avanzada/Clase-N03-Ejercicio3.py
@@ -0,0 +1,40 @@
+#Definicion de la clase Punto
+class Punto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+#Definicion de la clase Linea
+class Linea:
+    def __init__(self, punto_inicio, punto_fin):
+        self._punto_inicio = punto_inicio
+        self._punto_fin = punto_fin
+
+    def mueve_derecha(self, distancia):
+        self._punto_inicio.x += distancia
+        self._punto_fin.x += distancia
+
+    def mueve_izquierda(self, distancia):
+        self._punto_inicio.x -= distancia
+        self._punto_fin.x -= distancia
+
+    def mueve_arriba(self, distancia):
+        self._punto_inicio.y += distancia
+        self._punto_fin.y += distancia
+
+    def mueve_abajo(self, distancia):
+        self._punto_inicio.y -= distancia
+        self._punto_fin.y -= distancia
+
+
+punto_inicio = Punto(3, 4)
+punto_fin = Punto(5, 4)
+linea = Linea(punto_inicio, punto_fin)
+
+print("Punto de inicio:", (linea._punto_inicio.x, linea._punto_inicio.y))
+print("Punto de fin:", (linea._punto_fin.x, linea._punto_fin.y))
+
+linea.mueve_derecha(2)
+print("Desplazamiento a la derecha:")
+print("Punto de inicio:", (linea._punto_inicio.x, linea._punto_inicio.y))
+print("Punto de fin:", (linea._punto_fin.x, linea._punto_fin.y))
