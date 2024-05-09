4a0e039141f15d8b83ee4cfa72e3bc4d8c8a022c
Ramiro <ramirosimari26@gmail.com>
2024-05-08 22:51:41 -0300

    Agrego Ejercicio02

---
+++ Programación-Avanzada/ClaseN04/Clase-N04-Ejercicio02.py
@@ -0,0 +1,8 @@
+"""
+Ejercicio 2: Definir una funcion denominada imprimir_mensaje que imprima el siguiente mensaje en pantalla:
+"Estudiando Fundamentos de Informatica en la UNAB". No recibe ninguna informacion por lo tanto no tiene ningun parametro.
+"""
+def imprimir_mensaje():
+    print('Estudiando Fundamentos de Informatica en la UNAB.')
+    
+imprimir_mensaje()
